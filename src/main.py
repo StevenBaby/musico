d0df2c86b0b1671297c539a768ffc06cfc3e6639
Steven Kang <kangweibaby@163.com>
2023-08-25 12:43:58 +0800

    add mido

--- src/main.py
+++ src/main.py
@@ -8,19 +8,37 @@ from PySide6 import (
     QtUiTools,
 )
 
+import mido
+
 
 from logger import logger
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 assets = os.path.abspath(os.path.join(dirname, "../assets"))
 
+
 class MainWindow(QtWidgets.QMainWindow):
+
+    def midi_input_callback(self, msg):
+        logger.debug("input message %s", msg)
+        self.outport.send(msg)
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
 
         self.setWindowIcon(QtGui.QIcon(os.path.join(assets, "favicon.ico")))
         self.setWindowTitle("Musico")
+
+        logger.debug("output")
+        for name in mido.get_output_names():
+            logger.debug(name)
+
+        logger.debug("input")
+        for name in mido.get_input_names():
+            logger.debug(name)
+
+        # self.inport = mido.open_input(callback=self.midi_input_callback)
+        # self.outport = mido.open_output()
 
 
 def main():
